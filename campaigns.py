8eef7b24f0e11eb46e04fcadc69f419b730c051c
launchsites <james@launchsites.uk>
2026-03-07 23:28:28 +0000

    selection for campaigns

--- campaigns.py
+++ campaigns.py
@@ -5,5 +5,19 @@
     # remove campaigns
     # list data of campaign
 
-def campaign():
-    print("campaign")
+def campaign(command: str, option: str | None = None):
+
+    if command == "create":
+        # handle creating one with name option
+
+    elif command == "select":
+        # handle selectingi the one with name option
+
+    elif command == "list":
+        if option:
+            #handle listing the data from campaign
+        else:
+            # list all the campaigns
+
+    elif command == "remove":
+        # remove the campaign
