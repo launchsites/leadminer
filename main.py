c9dad0b7247b5c8a842378e3b175fde05e066d7c
launchsites <james@launchsites.uk>
2026-03-08 03:16:44 +0000

    trying to build

--- main.py
+++ main.py
@@ -9,19 +9,19 @@ from setup import setup
 from help import help
 from campaigns import campaign
 
+def nit():
+    # check that .env exists otherwise create
+    if not os.path.exists(".env"):
+       Path(".env").touch()
 
-# check that .env exists otherwise create
-if not os.path.exists(".env"):
-   Path(".env").touch()
+    load_dotenv(".env")
 
-load_dotenv(".env")
+    searchLimit = os.getenv("LIMIT")
+    places_api_key = os.getenv("API_KEY")
 
-searchLimit = os.getenv("LIMIT")
-places_api_key = os.getenv("API_KEY")
-
-# if no limit set
-if searchLimit is None:
-    set_key(".env", "LIMIT", "100")
+    # if no limit set
+    if searchLimit is None:
+        set_key(".env", "LIMIT", "100")
 
 app = typer.Typer()
 
@@ -32,5 +32,5 @@ app.command()(help)
 app.command()(campaign)
 app.command()(setup)
 
-
+nit()
 app()
